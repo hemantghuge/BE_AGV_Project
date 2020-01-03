cb48715bddafbe0b7026f32c30a47fee83447d91
HEMANT GORAKSH GHUGE <38520505+HemantGorakshGhuge@users.noreply.github.com>
2020-01-03 18:13:16 +0530

    Optimised algorithm
    It will overwrite the image

--- resize.py
+++ resize.py
@@ -1,4 +1,5 @@
 # Python3 remaining all the images of a folder
+# WARNING - Exisitng photos will be overwritten so make sure to have a backup folder
   
 # importing os module 
 import os 
@@ -7,32 +8,33 @@ import numpy as np
 import time
   
 # Function to rename multiple files 
-def main():
-    
+def main(): 
     scale_percent = 95 # percent of original size
-
+      
     for filename in os.listdir("Photos"):
 
-        img = cv2.imread(os.path.join("Photos",filename))
-        height = np.size(img, 0)
-        width = np.size(img, 1)
-        original_size=(height*width)/(8*1000)
-        print(filename+' '+str(original_size))
+        img = cv2.imread(os.path.join("Photos",filename)) #reading images from the folder
+        height = np.size(img, 0) #height of image
+        width = np.size(img, 1) #width of image
+        
+        original_size = os.path.getsize(os.path.join("Photos",filename))#calculating size in bytes
+        original_size = original_size/1024  #converting into kilo bytes
+        print(filename+' '+str(original_size)) 
 
-        size = original_size
+        size = original_size 
         resize_img = img
 
         while not(size < 300):
             
-            width = int(resize_img.shape[1] * scale_percent / 100)
-            height = int(resize_img.shape[0] * scale_percent / 100)
+            width = int(resize_img.shape[1] * scale_percent / 100) #finding width value
+            height = int(resize_img.shape[0] * scale_percent / 100) #finding height value
             dim = (width, height)
-            size = (height*width)/(8*1000)
+            size = os.path.getsize(os.path.join("Photos",filename)) #calculating size in bytes
+            size = size/1024;   #converting into kilo bytes
             # resize image
             resize_img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
-            print(filename+'Resizing'+str(size))
-        
-        cv2.imwrite("small"+filename,resize_img)    
-  
+            print(filename+' Resizing '+str(size))
+            cv2.imwrite("Photos/"+filename,resize_img)  #saving the image
+
 if __name__ == '__main__': 
     main() 
