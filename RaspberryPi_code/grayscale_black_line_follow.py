9d82d8fdfb51c8ed204ee23f8b4c7945b0884c0e
HemantGorakshGhuge <hemantgghuge@gmail.com>
2020-01-14 23:15:39 +0530

    RGB to Grayscale

--- RaspberryPi_code/grayscale_black_line_follow.py
+++ RaspberryPi_code/grayscale_black_line_follow.py
@@ -11,7 +11,23 @@ video_capture.set(3,width)
 video_capture.set(4,height)
 video_capture.set(5,fps)
 
+fourcc = cv.VideoWriter_fourcc(*'XVID')
+video_capture.set(cv.CAP_PROP_FOURCC,cv.VideoWriter_fourcc(*'MJPG'))
 
 
+while(video_capture.isOpened()):
+    
+    start = time.time()
+    
+    ret, frame = video_capture.read()
 
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
+    cv.imshow('frame', frame)
+    cv.imshow('gray', gray)
+
+    if cv.waitKey(1) & 0x77 == ord('q'):
+        break
+
+video_capture.release()
+cv.destroyAllWindows()
