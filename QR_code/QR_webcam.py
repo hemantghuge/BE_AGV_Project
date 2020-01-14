e1dd27b61c7d8e805e34c3735f9be89891c394a4
Unnati_Kulkarni <53504262+Unnatirk@users.noreply.github.com>
2020-01-14 17:33:11 +0530

    code to read QR code using webcam

---
+++ QR_code/QR_webcam.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+import pyzbar.pyzbar as pyzbar
+
+cap = cv2.VideoCapture(0)
+font = cv2.FONT_HERSHEY_PLAIN
+
+while True:
+    _, frame = cap.read()
+
+    decodedObjects = pyzbar.decode(frame)
+    for obj in decodedObjects:
+        #print("Data", obj.data)
+        cv2.putText(frame, str(obj.data), (50, 50), font, 2,
+                    (255, 0, 0), 3)
+
+    cv2.imshow("Frame", frame)
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break26
