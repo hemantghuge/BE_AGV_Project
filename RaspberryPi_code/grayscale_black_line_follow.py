410a254bff8525ca188d661d675549b2ae80a169
HemantGorakshGhuge <hemantgghuge@gmail.com>
2020-01-15 22:36:36 +0530

    Saving Video

--- RaspberryPi_code/grayscale_black_line_follow.py
+++ RaspberryPi_code/grayscale_black_line_follow.py
@@ -2,8 +2,7 @@ import numpy as np
 import cv2 as cv
 import time
 
-#16:9
-width = 854 
+width = 640
 height = 480
 fps = 30
 percent = 100
@@ -14,7 +13,10 @@ video_capture.set(4,height)
 video_capture.set(5,fps)
 
 fourcc = cv.VideoWriter_fourcc(*'XVID')
+out = cv.VideoWriter('videoAfterProcessing.avi',fourcc, fps, (width,height))
+out1 = cv.VideoWriter('videoThreshold.avi', fourcc, fps, (width,height))
 video_capture.set(cv.CAP_PROP_FOURCC,cv.VideoWriter_fourcc(*'MJPG'))
+
 
 def rescale_frame(frame, percent):
     width = int(frame.shape[1]*percent/100)
@@ -54,8 +56,8 @@ while(video_capture.isOpened()):
         cx = int(M['m10']/M['m00'])
         cy = int(M['m01']/M['m00'])
 
-        cv.line(crop_img,(cx,0),(cx,height), (255,0,0),1)
-        cv.line(crop_img,(0,cy),(width,cy), (255,0,0),1)
+        cv.line(crop_img,(cx,0),(cx,height), (255,0,0),3)
+        cv.line(crop_img,(0,cy),(width,cy), (255,0,0),3)
 
         cv.drawContours(crop_img, con, -1, (0,255,0), 3) #con=max(contours)
 
@@ -75,6 +77,9 @@ while(video_capture.isOpened()):
         print ('I don\'t see the contour') 
 
     show_graphics()
+    
+    out.write(crop_img)
+    out1.write(thresh)
 
     end = time.time()
     print("time execution " + str(end-start))
@@ -83,5 +88,7 @@ while(video_capture.isOpened()):
     if cv.waitKey(1) & 0x77 == ord('q'):
         break
 
-video_capture.release()
+#video_capture.release()
+out.release()
+out1.release()
 cv.destroyAllWindows()
