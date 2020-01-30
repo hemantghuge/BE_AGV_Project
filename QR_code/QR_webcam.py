0da258835bffb3d1cf61cfe0814059b32a8b4f7e
Unnati_Kulkarni <53504262+Unnatirk@users.noreply.github.com>
2020-01-30 13:50:20 +0530

    Comments added

--- QR_code/QR_webcam.py
+++ QR_code/QR_webcam.py
@@ -1,25 +1,63 @@
-#Use https://www.the-qrcode-generator.com/ website to generate QR code.
+#use https://www.the-qrcode-generator.com/ website to generate QR code.
+"""
+Project Name: Smart and Efficient Techniques for Automated Guided Vehicles
+Author List: Unnati
+Filename: qr_web_01.py
+Functions: cap.read(), pyzbar.decode(frame), cap.release(), cv2.destroyAllWindows()
+Libraries: pyzbar, numpy
+"""
 
 import cv2
 import numpy as np
 import pyzbar.pyzbar as pyzbar
 
-cap = cv2.VideoCapture(0)
-font = cv2.FONT_HERSHEY_PLAIN
+cap = cv2.VideoCapture(0) #open the default camera that is webcam
+font = cv2.FONT_HERSHEY_PLAIN #font to be displayed on screen
 
+#continuous loop 
 while True:
+   
+#Function: cap.read
+#Input: None
+#Logic: Returns True if frame is read correctly.
+    
     _, frame = cap.read()
 
+
+#Function: pyzbar.decode
+#Input: frame
+#Output: decoded object (QR code)
+#Logic: Return symbols from the frame
+    
     decodedObjects = pyzbar.decode(frame)
     for obj in decodedObjects:
-        #print("Data", obj.data)
+        
         cv2.putText(frame, str(obj.data), (50, 50), font, 2,
-                    (255, 0, 0), 3)
+                    (255, 0, 0), 3)  #inserting text on the frame
 
-    cv2.imshow("Frame", frame)
-
-    if cv2.waitKey(1) & 0XFF == ord('q'):
+    cv2.imshow("Frame", frame)  #display image with QR text
+    
+    if cv2.waitKey(1) & 0XFF == ord('q'): 
         break
 
+"""
+
+Function: cap.release
+Input: None
+Output: None
+Logic: release camera object
+
+"""
+
 cap.release()
-cv2.destroyAllWindows()        
+
+"""
+
+Function: destroyAllWindows
+Input: None
+Output: None
+Logic: closing all the windows
+
+"""
+
+cv2.destroyAllWindows()
