e9565c82b0a027443c616b864cf5c0e700dfb9b7
HemantGorakshGhuge <hemantgghuge@gmail.com>
2020-02-01 14:35:28 +0530

    Update code with respct to Raspi4

--- RaspberryPi_code/QR_code_webcam_raspi.py
+++ RaspberryPi_code/QR_code_webcam_raspi.py
@@ -3,25 +3,41 @@
 Project Name: Smart and Efficient Techniques for Automated Guided Vehicles
 Author List: Unnati R Kulkarni
 Filename: QR_webcam.py
-Functions: cap.read(), pyzbar.decode(frame), cap.release(), cv2.destroyAllWindows()
+Functions: video_capture.read(), pyzbar.decode(frame), video_capture.release(), cv2.destroyAllWindows()
 Libraries: pyzbar, numpy
 """
 
-import cv2
+import cv2 as cv
 import numpy as np
 import pyzbar.pyzbar as pyzbar
+import time
 
-cap = cv2.VideoCapture(0) #open the default camera that is webcam
-font = cv2.FONT_HERSHEY_PLAIN #font to be displayed on screen
+width = 640
+height = 480
+fps = 30
+percent = 100
+
+video_capture = cv.VideoCapture(0) #open the default camera that is webcam
+video_capture.set(3,width)
+video_capture.set(4,height)
+video_capture.set(5,fps)
+
+fourcc = cv.VideoWriter_fourcc(*'XVID')
+#out = cv.VideoWriter('videoAfterProcessing.avi',fourcc, fps, (width,height))
+#out1 = cv.VideoWriter('videoThreshold.avi', fourcc, fps, (width,height))
+video_capture.set(cv.CAP_PROP_FOURCC,cv.VideoWriter_fourcc(*'MJPG'))
+
+#video_capture = cv2.Videovideo_captureture(0) #open the default camera that is webcam
+font = cv.FONT_HERSHEY_PLAIN #font to be displayed on screen
 
 #continuous loop 
 while True:
    
-#Function: cap.read
+#Function: video_capture.read
 #Input: None
 #Logic: Returns True if frame is read correctly.
     
-    _, frame = cap.read()
+    _, frame = video_capture.read()
 
 
 #Function: pyzbar.decode
@@ -32,24 +48,24 @@ while True:
     decodedObjects = pyzbar.decode(frame)
     for obj in decodedObjects:
         
-        cv2.putText(frame, str(obj.data), (50, 50), font, 2,
+        cv.putText(frame, str(obj.data), (50, 50), font, 2,
                     (255, 0, 0), 3)  #inserting text on the frame
 
-    cv2.imshow("Frame", frame)  #display image with QR text
+    cv.imshow("Frame", frame)  #display image with QR text
     
-    if cv2.waitKey(1) & 0XFF == ord('q'): 
+    if cv.waitKey(1) & 0XFF == ord('q'): 
         break
 
 """
 
-Function: cap.release
+Function: video_capture.release
 Input: None
 Output: None
 Logic: release camera object
 
 """
 
-cap.release()
+video_capture.release()
 
 """
 
@@ -60,5 +76,5 @@ Logic: closing all the windows
 
 """
 
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
 
