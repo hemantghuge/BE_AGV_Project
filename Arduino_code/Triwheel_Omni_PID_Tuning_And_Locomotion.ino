082ba0cb6b9f105024e80cff724dc2ce0fa5902a
aadeshborate <40480399+aadeshborate@users.noreply.github.com>
2020-02-01 23:53:21 +0530

    Update Triwheel_Omni_PID_Tuning_And_Locomotion.ino

--- Arduino_code/Triwheel_Omni_PID_Tuning_And_Locomotion.ino
+++ Arduino_code/Triwheel_Omni_PID_Tuning_And_Locomotion.ino
@@ -1,8 +1,11 @@
+// Inclusion of libraries.
 #include<math.h>
 #include <Wire.h>
 #include <I2Cdev.h>
 #include <L3GD20H.h>
 #include <LiquidCrystal.h>
+
+// GPIO pin declarations.
 int PWM_F = 44;
 int PWM_L = 46;
 int PWM_R = 45;
@@ -23,26 +26,18 @@ int Red = 24;
 int Blue = 25;
 int VL = 0;
 int Buzzer = 49;
+int servo = 12;
+int count = 1;
+// LCD pin declaration
 LiquidCrystal lcd(26, 27, 28, 29, 30, 31);
-float reading = 0;
-char SER_IP = 'S';
-// specific I2C address may be passed here
+// Object declarations
 L3GD20H gyro;
-
+// Variable Declarations
 int16_t avx, avy, avz;
-bool blinkState = false;
-float delta = 0, threshold = 0;
 long int timer , tp;
 float timestep, yaw;
-void Clockwise();
-void AntiClockwise();
-void V1_Clockwise();
-void V2_Clockwise();
-void V3_Clockwise();
-void V1_AntiClockwise();
-void V2_AntiClockwise();
-void V3_AntiClockwise();
-
+float reading = 0;
+char SER_IP = 'S';
 float theta = 0 * M_PI / 180;
 float V1, V2, V3;
 float Vx, Vy;
@@ -56,9 +51,26 @@ float kp = 15;
 float ki = 0.001;
 float kd = 161 ;
 float setpoint = 0;
-int count;
+// Function declarations
+void Clockwise();
+void AntiClockwise();
+void V1_Clockwise();
+void V2_Clockwise();
+void V3_Clockwise();
+void V1_AntiClockwise();
+void V2_AntiClockwise();
+void V3_AntiClockwise();
 void setup()
 {
+  /*
+    Function Name:   pinMode(pin, mode)
+    Input:    pin: the Arduino pin number to set the mode of.
+              mode: INPUT, OUTPUT, or INPUT_PULLUP.
+    Output:     Nothing
+    Logic:    The code makes the digital "pin" available for either INPUT/INPUT_PULLUP or OUTPUT.
+    Example Call:   pinMode(PWM_F, OUTPUT);
+  */
+
   pinMode(PWM_F, OUTPUT);
   pinMode(DR_F1, OUTPUT);
   pinMode(DR_F2, OUTPUT);
@@ -72,8 +84,10 @@ void setup()
   pinMode(Red, OUTPUT);
   pinMode(Blue, OUTPUT);
   pinMode(Buzzer, OUTPUT);
+  pinMode(servo, OUTPUT);
   lcd.begin(16, 2);
   digitalWrite(Buzzer, HIGH);
+
   Wire.begin();
   Serial.begin(115200);
   Serial1.begin(9600);
@@ -117,45 +131,64 @@ void loop() {
   if (SER_IP == 'F')
   {
     theta = 90 * M_PI / 180;
+    count = 1;
   }
   if (SER_IP == 'B')
   {
     theta = 270 * M_PI / 180;
+    count = 1;
   }
   if (SER_IP == 'L')
   {
     theta = 180 * M_PI / 180;
+    count = 1;
   }
   if (SER_IP == 'R')
   {
     theta = 0 * M_PI / 180;
+    count = 1;
   }
   if (SER_IP == 'G')
   {
     theta = 135 * M_PI / 180;
+    count = 1;
   }
   if (SER_IP == 'I')
   {
     theta = 45 * M_PI / 180;
+    count = 1;
   }
   if (SER_IP == 'H')
   {
-    theta = 225 * M_PI / 180;
+    //    theta = 225 * M_PI / 180;
+    //    count = 1;
+    digitalWrite(servo, HIGH);
+    delayMicroseconds(2300);
+    digitalWrite(servo, LOW);
+    delay(20);
+
   }
   if (SER_IP == 'J')
   {
-    theta = 315 * M_PI / 180;
+    //    theta = 315 * M_PI / 180;
+    //    count = 1;
+    digitalWrite(servo, HIGH);
+    delayMicroseconds(1500);
+    digitalWrite(servo, LOW);
+    delay(20);
   }
 
   if (SER_IP >= '1' && SER_IP <= '9')
   {
     VL = SER_IP ;
+    count = 1;
   }
   if (SER_IP == 'W')
   {
     digitalWrite(Green, HIGH);
     digitalWrite(Red, HIGH);
     digitalWrite(Blue, HIGH);
+    count = 1;
   }
   if (SER_IP == 'w')
   {
@@ -163,28 +196,34 @@ void loop() {
     digitalWrite(Green, LOW);
     digitalWrite(Red, LOW);
     digitalWrite(Blue, LOW);
+    count = 1;
 
   }
   if (SER_IP == 'U')
   {
 
     digitalWrite(Red, HIGH);
+    count = 1;
 
   }
   if (SER_IP == 'u')
   {
 
     digitalWrite(Red, LOW);
+    count = 1;
 
   }
   if (SER_IP == 'V')
   {
     digitalWrite(Buzzer, LOW);
+    count = 1;
   }
   if (SER_IP == 'v')
   {
     digitalWrite(Buzzer, HIGH);
+    count = 1;
   }
+
   V = map(VL , 49, 58, 0, 255);
   if (V == -1388)
   {
@@ -241,11 +280,15 @@ void loop() {
   {
     V3_AntiClockwise();
   }
-//  digitalWrite(Green, HIGH);
-//    digitalWrite(Red, HIGH);
-//   digitalWrite(Blue, HIGH);
-}
 
+}
+/*
+    Function Name: V1_Clockwise()
+    Input: None
+    Output: None
+    Logic: For driving motor in Clockwise direction.
+    Example Call: V1_Clockwise()
+*/
 void V1_Clockwise()
 {
   analogWrite(PWM_F, abs(V1));
@@ -253,6 +296,13 @@ void V1_Clockwise()
   digitalWrite(DR_F2, LOW);
 
 }
+/*
+    Function Name: V2_Clockwise()
+    Input: None
+    Output: None
+    Logic: For driving motor in Clockwise direction.
+    Example Call: V2_Clockwise()
+*/
 void V2_Clockwise()
 {
 
@@ -262,6 +312,13 @@ void V2_Clockwise()
 
 
 }
+/*
+    Function Name: V3_Clockwise()
+    Input: None
+    Output: None
+    Logic: For driving motor in Clockwise direction.
+    Example Call: V3_Clockwise()
+*/
 void V3_Clockwise()
 {
 
@@ -270,35 +327,44 @@ void V3_Clockwise()
   digitalWrite(DR_R2, LOW);
 
 }
+/*
+    Function Name: V1_AntiClockwise()
+    Input: None
+    Output: None
+    Logic: For driving motor in Clockwise direction.
+    Example Call: V1_AntiClockwise();
+*/
+
 void V1_AntiClockwise()
 {
   analogWrite(PWM_F, abs(V1));
   digitalWrite(DR_F1, LOW);
   digitalWrite(DR_F2, HIGH);
 }
+/*
+    Function Name: V2_AntiClockwise()
+    Input: None
+    Output: None
+    Logic: For driving motor in Clockwise direction.
+    Example Call: V2_AntiClockwise();
+*/
 void V2_AntiClockwise()
 {
 
   analogWrite(PWM_L, abs(V2));
   digitalWrite(DR_L1, LOW);
   digitalWrite(DR_L2, HIGH);
-
 }
+/*
+    Function Name: V3_AntiClockwise()
+    Input: None
+    Output: None
+    Logic: For driving motor in Clockwise direction.
+    Example Call: V3_AntiClockwise();
+*/
 void V3_AntiClockwise()
 {
   analogWrite(PWM_R, abs(V3));
   digitalWrite(DR_R1, LOW);
   digitalWrite(DR_R2, HIGH);
 }
-//void AntiClockwise()
-//{
-//  analogWrite(PWM_F, abs(V1) );
-//  digitalWrite(DR_F1, LOW);
-//  digitalWrite(DR_F2, HIGH);
-//  analogWrite(PWM_L, abs(V2));
-//  digitalWrite(DR_L1, LOW);
-//  digitalWrite(DR_L2, HIGH);
-//  analogWrite(PWM_R, abs(V3) );
-//  digitalWrite(DR_R1, LOW);
-//  digitalWrite(DR_R2, HIGH);
-//}
