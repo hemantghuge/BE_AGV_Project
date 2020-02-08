e64b0ceaf425ba7022b7ff4735da9216da5147f5
Unnati_Kulkarni <unnatikulkarni46@gmail.com>
2020-02-08 18:14:42 +0530

    Serial communication test completed

--- Arduino_code/serial_comm_arduino_pi/serial_comm_arduino_pi.ino
+++ Arduino_code/serial_comm_arduino_pi/serial_comm_arduino_pi.ino
@@ -1,15 +1,17 @@
 byte number = 0;
 
+int num;
 void setup(){
 Serial3.begin(9600);
 Serial.begin(9600);
+Serial3.setTimeout(10);
 }
 
 void loop(){
 if (Serial3.available()) {
 String number = Serial3.readString();
 int num = number.toInt();
-Serial.print("character recieved: ");
+Serial.println("character recieved: ");
 Serial.println(number);
 Serial.println(num);
 }
