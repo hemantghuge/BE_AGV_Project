c39ae6fe5471a646f6c848775b7c6c81e352c9eb
HemantGorakshGhuge <hemantgghuge@gmail.com>
2020-02-19 00:12:32 +0530

    Multi serial receive data added

--- Arduino_code/serial_comm_arduino_pi/serial_comm_arduino_pi.ino
+++ Arduino_code/serial_comm_arduino_pi/serial_comm_arduino_pi.ino
@@ -1,6 +1,9 @@
-byte number = 0;
+byte angle = 0;
+int ang;
 
-int num;
+byte sign = 0;
+int sig;
+
 void setup(){
 Serial3.begin(9600);
 Serial.begin(9600);
@@ -9,10 +12,14 @@ Serial3.setTimeout(10);
 
 void loop(){
 if (Serial3.available()) {
-String number = Serial3.readString();
-int num = number.toInt();
-Serial.println("character recieved: ");
-Serial.println(number);
-Serial.println(num);
+String angle = Serial3.readString();
+int ang = angle.toInt();
+delay(20);
+String sign = Serial3.readString();
+int sig = sign.toInt();
+Serial.println("character received: ");
+Serial.println(ang);
+Serial.println(sig);
+
 }
 }
